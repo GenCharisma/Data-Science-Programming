a321a9aaae9f05944c04fb14837625fd6815b530
PITCH30391\User <pitchnimit@hotmail.com>
2021-07-26 23:21:52 +0700

    start to work on numbers

--- LargestPossibleNumberOfFourIntegers.py
+++ LargestPossibleNumberOfFourIntegers.py
@@ -1,1 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 26 21:36:34 2021
 
+"""
+
+x = list(map(str, input("Enter Four Numbers : ").split()))
+
+for i in x:
+    print(i)
+
+y = x[0] + x[1]

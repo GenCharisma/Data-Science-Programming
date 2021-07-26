3dbc95551169026aa15b60dfa7a47ee1a0f26e5e
PITCH30391\User <pitchnimit@hotmail.com>
2021-07-27 00:31:06 +0700

    attempt to detect high number

--- LargestPossibleNumberOfFourIntegers.py
+++ LargestPossibleNumberOfFourIntegers.py
@@ -4,9 +4,13 @@ Created on Mon Jul 26 21:36:34 2021
 
 """
 
-x = list(map(str, input("Enter Four Numbers : ").split()))
+numbers = list(map(str, input("Enter Four Numbers : ").split()))
 
-for i in x:
-    print(i)
+for a in numbers:
+    for b in numbers:
+        for c in numbers:
+            for d in numbers:
+                if (a != b and a != c and a != d and b != c and b != d and c != d):
+                    a+b+c+d
+                    print(a+b+c+d)
 
-y = x[0] + x[1]

c2de4beabbf9900ff2c4809c1e0a78a6c0a21759
GenCharisma\User <pitchnimit@hotmail.com>
2021-08-08 00:21:03 +0700

    add plot

--- IceCreamSolving.py
+++ IceCreamSolving.py
@@ -17,16 +17,15 @@ Created on Sat Aug  7 11:35:45 2021
 # that you would like to produce to get maximum profit ?
 # Use Python to find the answer ; provide solving explanation in PDF file
 
-# ice cream type
 # xi = the number of ice cream type (1 : vanilla, 2 : strawberry)
 #    x1, x2
 
 # Objective
     # max : 2(x1) + 3(x2)
 # Contraints
-    # Vanilla :     0.5(x1) + 0.2(x2) <= 10
-    # Strawberry :  1(x1)   + 1(x2)   <= 30
-    #               x1,x2             >= 0
+    # Ice Creams Limit : 0.5(x1) + 0.2(x2) <= 10
+    # Order Limit      : 1(x1)   + 1(x2)   <= 30
+    #                    x1,x2             >= 0
 
 import numpy as np
 from scipy.linalg import solve
@@ -36,5 +35,20 @@ x = solve (A,b)
 print(x)
 xrounded = np.around(x)
 
-best = (2*xrounded[0]) + (3*xrounded[1])
+print("all vanilla", (2*0) + (3*30))
+print("all strawberry",(2*30) + (3*0))
+print("Crosspoint", (2*xrounded[0]) + (3*xrounded[1]))
 
+import matplotlib.pyplot as plt
+f, ax = plt.subplots()
+plt.plot([x[0]],[x[1]],'ro')
+plt.plot([20,0],[0,50])
+plt.plot([30,0],[0,30])
+plt.grid()
+ax.set_ylim(bottom=0)
+ax.set_xlim(left=0)
+#plt.fill_between([20,0], [x[1]], [0,30])
+plt.fill_between([0,x[0],20],[30,x[1],0],0, alpha = 0.5)
+plt.ylabel('Strawberry')
+plt.xlabel('Vanilla')
+plt.show()

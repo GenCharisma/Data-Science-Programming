1e80088ce89674cc6f52181e97c2313edabe984a
GenCharisma\User <pitchnimit@hotmail.com>
2021-08-08 02:29:52 +0700

    fix typo, add more texts

--- IceCreamSolving.py
+++ IceCreamSolving.py
@@ -17,12 +17,13 @@ Created on Sat Aug  7 11:35:45 2021
 # that you would like to produce to get maximum profit ?
 # Use Python to find the answer ; provide solving explanation in PDF file
 
-# xi = the number of ice cream type (1 : vanilla, 2 : strawberry)
-#    x1, x2
+# Decision Variables
+    # xi = the number of ice cream type (1 : vanilla, 2 : strawberry)
+    #    x1, x2
 
 # Objective
     # max : 2(x1) + 3(x2)
-# Contraints
+# Constraints
     # Ice Creams Limit : 0.5(x1) + 0.2(x2) <= 10
     # Order Limit      : 1(x1)   + 1(x2)   <= 30
     #                    x1,x2             >= 0
@@ -35,18 +36,14 @@ x = solve (A,b)
 print(x)
 xrounded = np.around(x)
 
-print("all vanilla", (2*0) + (3*30))
-print("all strawberry",(2*20) + (3*0))
-print("Crosspoint", (2*xrounded[0]) + (3*xrounded[1]))
-
 import matplotlib.pyplot as plt
 f, ax = plt.subplots()
-plt.plot([x[0]],[x[1]],'ro')
-plt.plot([20,0],[0,50])
-plt.plot([30,0],[0,30])
+plt.plot([20,0],[0,50],label="Ice Creams Limit")
+plt.plot([30,0],[0,30],label="Order Limit")
 xArea = [0,x[0],20]
 yArea = [30,x[1],0]
 nArea = ['(0, 30)','(13, 17)','(20, 0)']
+plt.plot([x[0]],[x[1]],'ro')
 plt.fill_between(xArea,yArea,0, alpha = 0.4)
 for i, txt in enumerate(nArea):
     ax.annotate(txt, (xArea[i], yArea[i]), xytext=(5,5), textcoords='offset points')
@@ -55,4 +52,9 @@ ax.set_xlim(left=0)
 plt.grid()
 plt.ylabel('Strawberry')
 plt.xlabel('Vanilla')
+plt.legend()
 plt.show()
+
+print("All Vanilla, 20 Vanilla is maximum :", (2.*20.) + (3.*0.), "USD")
+print("All Strawberry, 30 Strawberry is maximum :", (2.*0.) + (3.*30.), "USD")
+print("Mix Crosspoint 13 Vanilla and 17 Strawberry :", (2*xrounded[0]) + (3*xrounded[1]), "USD")
